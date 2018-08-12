b3ed14606679e7aa1367c821e6eea6f916ed14e2
thomas <thomas.treffry@nearform.com>
2018-08-12 18:54:14 +0200

    Green cursor

--- config.h
+++ config.h
@@ -107,8 +107,8 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  "#cccccc",
+  "#555555",
 };
 
 
@@ -118,8 +118,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 2;
+static unsigned int defaultrcs = 2;
 
 /*
  * Default shape of cursor
